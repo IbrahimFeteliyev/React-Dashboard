6e0706f776fa0e1a424a50e8a0089caf65a81272
İbrahim Fətəliyev <ibrahim.fataliyev.c@asoiu.edu.az>
2022-07-01 12:50:43 +0400

    first-commit

---
+++ src/components/Login/Login.jsx
@@ -0,0 +1,52 @@
+import { FormControl, TextField, Button, Avatar } from '@mui/material'
+import React, { useEffect, useState } from 'react'
+import LockOutlinedIcon from '@mui/icons-material/LockOutlined';
+import './login.scss'
+import { useDispatch, useSelector } from 'react-redux';
+import { useNavigate } from 'react-router-dom';
+import { loginUserAction } from '../../redux/Actions/LoginAdminAction'
+
+const Login = () => {
+    const { userInfo } = useSelector((state) => state.user)
+    const [email, setEmail] = useState("")
+    const [password, setPassword] = useState("")
+
+    const navigate = useNavigate()
+    const dispach = useDispatch()
+
+    const loginHandler = () => {
+        dispach(loginUserAction(email, password))
+    }
+    useEffect(() => {
+    }, [navigate])
+
+    if (userInfo.length !== 0) {
+        navigate("/")
+    }
+   
+    console.log(userInfo);
+
+
+
+    return (
+        <section className='login-section'>
+            <div className="container">
+                <div className="login-box m-auto">
+                    <Avatar className='sign-in-avatar m-auto mb-2'><LockOutlinedIcon /></Avatar>
+                    <h3 className='text-center mb-4 m-auto'>Sign in</h3>
+                    <FormControl className="my-2" fullWidth>
+                        <TextField onChange={(e) => setEmail(e.target.value)} id="outlined-basic" label="Email Address *" variant="outlined" />
+                    </FormControl>
+                    <FormControl className="my-2" fullWidth>
+                        <TextField onChange={(e) => setPassword(e.target.value)} id="outlined-basic" type="Password" label="Password *" variant="outlined" />
+                    </FormControl>
+                    <Button onClick={() => loginHandler()} className="my-3" fullWidth variant="contained" color="primary">
+                        Sign In
+                    </Button>
+                </div>
+            </div>
+        </section>
+    )
+}
+
+export default Login
---
+++ src/pages/listItems.js
@@ -0,0 +1,72 @@
+import * as React from 'react';
+import ListItemButton from '@mui/material/ListItemButton';
+import ListItemIcon from '@mui/material/ListItemIcon';
+import ListItemText from '@mui/material/ListItemText';
+import DashboardIcon from '@mui/icons-material/Dashboard';
+import PeopleIcon from '@mui/icons-material/People';
+import { Link } from 'react-router-dom';
+
+export const mainListItems = (
+  <React.Fragment>
+    <Link to="dashboard">
+    <ListItemButton>
+      <ListItemIcon>
+        <DashboardIcon />
+      </ListItemIcon>
+      <ListItemText primary="Dashboard" />
+    </ListItemButton>
+    </Link>
+    {/* <ListItemButton>
+      <ListItemIcon>
+        <ShoppingCartIcon />
+      </ListItemIcon>
+      <ListItemText primary="Auth" />
+    </ListItemButton> */}
+    <Link to="auth">
+    <ListItemButton >
+      <ListItemIcon>
+        <PeopleIcon />
+      </ListItemIcon>
+      <ListItemText primary="Auth" />
+    </ListItemButton>
+    </Link>
+    {/* <ListItemButton>
+      <ListItemIcon>
+        <BarChartIcon />
+      </ListItemIcon>
+      <ListItemText primary="Reports" />
+    </ListItemButton>
+    <ListItemButton>
+      <ListItemIcon>
+        <LayersIcon />
+      </ListItemIcon>
+      <ListItemText primary="Integrations" />
+    </ListItemButton> */}
+  </React.Fragment>
+);
+
+// export const secondaryListItems = (
+//   <React.Fragment>
+//     <ListSubheader component="div" inset>
+//       Saved reports
+//     </ListSubheader>
+//     <ListItemButton>
+//       <ListItemIcon>
+//         <AssignmentIcon />
+//       </ListItemIcon>
+//       <ListItemText primary="Current month" />
+//     </ListItemButton>
+//     <ListItemButton>
+//       <ListItemIcon>
+//         <AssignmentIcon />
+//       </ListItemIcon>
+//       <ListItemText primary="Last quarter" />
+//     </ListItemButton>
+//     <ListItemButton>
+//       <ListItemIcon>
+//         <AssignmentIcon />
+//       </ListItemIcon>
+//       <ListItemText primary="Year-end sale" />
+//     </ListItemButton>
+//   </React.Fragment>
+// );
